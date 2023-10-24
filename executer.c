2493a29a5678bc90f97702ff7d36be900dd3649a
Ana Blanco Mota <ablanco-@c2r7s2.42madrid.com>
2023-10-24 22:02:21 +0200

    only norminette

--- executer.c
+++ executer.c
@@ -6,7 +6,7 @@
 /*   By: ablanco- <ablanco-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 11:59:39 by ablanco-          #+#    #+#             */
-/*   Updated: 2023/10/12 17:08:08 by ablanco-         ###   ########.fr       */
+/*   Updated: 2023/10/24 22:01:49 by ablanco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,18 @@ void ft_exec(char *cmd, char **envp)
 	char **cmd_arg;
 	
 	cmd_arg = ft_split(cmd, ' ');
-	if (access(cmd, F_OK + X_OK) != 0) //esto está malm hacer Pepeforma buscando la / ./ ../ en argv, si tiene esto no hacer join
+	if (!strncmp(cmd_arg[0], "/", 1) || !strncmp(cmd_arg[0], "./", 2)
+		|| !strncmp(cmd_arg[0], "../", 3))
+		{
+			execve(cmd_arg[0], cmd_arg, envp);
+			perror(cmd_arg[0]);
+			exit(0);
+		}
+	else
 	{
 		fn_cmd = final_cmd(cmd_arg[0], envp);
 		execve(fn_cmd, cmd_arg, envp);
+		perror(cmd_arg[0]);
+		exit(0);
 	}
-	else
-		execve(cmd_arg[0], cmd_arg, envp);
 }
